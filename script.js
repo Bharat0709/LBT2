92dc71dce16470724cfd1353c47037c0c62e8254
Bharatpahwa <pahwabharat15@gmail.com>
2023-03-05 20:13:51 +0530

    AI Integration

--- script.js
+++ script.js
@@ -3,40 +3,76 @@
 
 const Scroll = document.querySelector("#Events-Section");
 const ScrollEvents = document.querySelector("#Events");
-
-const ResourceTypeHackathon = document.querySelector(
-  "#Resource-Type-Hackathon"
-);
-
-const ResourcesLinkHackathon = document.querySelector("#Hackathons-Link-List");
-const ResourcesLinkCoding = document.querySelector("#Coding-Link-List");
+const MagicSearch = document.querySelector(".MagicSearch");
+const Answer = document.querySelector(".answer");
 const Reload = document.querySelector(".Reload");
+const SearchContainer = document.querySelector(".search-container2");
+const SearchContainerFull = document.querySelector(".Searches");
+const SearchInput = document.querySelector(".search-input");
+const SearchButton = document.querySelector(".search");
+const SearchInput2 = document.querySelector(".search-input2");
+const SearchButton2 = document.querySelector(".search2");
+const SearchAgain = document.querySelector("#search-again");
+const SearchAgain2 = document.querySelector("#search-again2");
+const APIResponse = document.querySelector("#str");
+const Renderedurl = document.querySelector(".RenderedURL");
 
-const ArrowHacathons = document.querySelector("#Arrow-Id-Hackathons");
-const ArrowCoding = document.querySelector("#Arrow-Id-Coding");
 
-const HackathonImage = document.querySelector("#Hackathon");
-const CodingImage = document.querySelector("#Coding");
+
+
+MagicSearch.addEventListener("click", function () {
+  SearchContainerFull.style.display = "flex";
+});
+
+const RenderURl = function (data) {
+  const html = `
+  <a class="RenderedURL" href="${data.data[0].url}">Click Here To View</p>
+    `;
+  SearchContainer.insertAdjacentHTML("afterbegin", html);
+  console.log(data.data[0].url);
+};
 
 ScrollEvents.addEventListener("click", function () {
   Scroll.scrollIntoView({ behavior: "smooth" });
 });
 
-const SearchInput = document.querySelector(".search-input");
-const SearchButton = document.querySelector(".search");
+SearchAgain.addEventListener("click", function () {
+  SearchButton.style.display = "block";
+  SearchAgain.style.display = "none";
+  APIResponse.textContent = " ";
+  Answer.style.display = "none";
+});
+
+SearchAgain2.addEventListener("click", function () {
+  console.log(SearchInput2.value);
+  SearchButton2.style.display = "block";
+  SearchAgain2.style.display = "none";
+  Renderedurl.style.display = "none";
+  SearchInput2.style.display = "flex";
+  SearchInput2.value = " ";
+});
 
 SearchButton.addEventListener("click", function () {
+  alert("Sit Tight while we are generating your Answer");
   SearchButton.style.display = "none";
-  Reload.style.display = "flex";
   const PromptGiven = SearchInput.value;
+  SearchAgain.style.display = "flex";
+  Answer.style.display = "flex";
+
   const generateText = async (question) => {
-    const apiKey = ""; 
+    // const apiKey = "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";
     const url = `https://api.openai.com/v1/engines/text-davinci-003/completions`;
     const requestBody = {
       prompt: question,
       temperature: 0.9,
       max_tokens: 1000,
     };
+
+    if (SearchInput.value == "") {
+      alert("Nothing to Search!!");
+      Answer.style.display = "none";
+    }
+    SearchInput.value = "";
 
     const response = await fetch(url, {
       method: "POST",
@@ -49,14 +85,74 @@ SearchButton.addEventListener("click", function () {
 
     const json = await response.json();
     const generatedText = json.choices[0].text;
+    console.log(generateText);
 
     var string = `${generatedText}`;
     var str = string.split("");
     var el = document.getElementById("str");
+
     (function animate() {
       str.length > 0 ? (el.innerHTML += str.shift()) : clearTimeout(running);
       var running = setTimeout(animate, 30);
     })();
   };
+
   generateText(`${PromptGiven}`);
 });
+
+SearchButton2.addEventListener("click", function () {
+  alert("Sit Tight while we are generating your Image");
+  const PromptGiven2 = SearchInput2.value;
+  async function generateImage(prompt) {
+    const API_URL = "https://api.openai.com/v1/images/generations";
+    // const API_KEY = "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";
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "image-alpha-001",
+        prompt: prompt,
+        num_images: 1,
+      }),
+    });
+    const json = await response.json();
+    console.log(json.data[0].url);
+    RenderURl(json);
+    console.log("rendering done");
+  }
+
+  // Example usage:
+  generateImage(`${PromptGiven2}`)
+    .then((url) => console.log(url))
+    .catch((error) => console.error(error));
+
+  SearchAgain2.style.display = "flex";
+  SearchButton2.style.display = "none";
+  SearchInput2.style.display = "none";
+  Se;
+});
+
+// const EventsAPI = async (answer) => {
+//   const response = await fetch(
+//     `https://cdn.jsdelivr.net/npm/@openai/api@1.0.0/dist/openai-api.min.js`
+//   );
+//   const answer = response.data.choices[0].text;
+//   console.log(answer);
+// };
+
+// PYOBJNNIE6QUURITW3SO
+//https://private-anon-0737bf6d82-eventbriteapiv3public.apiary-mock.com/v3/venues/12345/
+//https://api.openai.com/v1/engines/text-davinci-003/completions
+
+// const EventsAPI = async (answer) => {
+//   const response2 = await fetch(
+//     `https://private-anon-0737bf6d82-eventbriteapiv3public.apiary-mock.com/v3/venues/12345/`
+//   );
+//   const json2 = await response2.json();
+//   console.log(json2);
+// };
+
+// EventsAPI();
